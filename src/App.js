275674021ffe18516aff12c6546022152ee35267
ayalena <82506563+ayalena@users.noreply.github.com>
2021-09-22 13:10:07 +0200

    opmaak changes

--- src/App.js
+++ src/App.js
@@ -17,84 +17,99 @@ function App() {
                 <div className="form-container">
                     <fieldset>
                         <legend>Gegevens</legend>
-                        <label htmlFor="name">Naam
-                            <input
-                                type="text"
-                                placeholder=""
-                                {...register("name", {
-                                    required: "Naam mag niet leeg zijn",
-                                    validate: {
-                                        value: (value) => value.includes('@'),
-                                        message: "Naam mag geen @ bevatten",
-                                    },
-                                })}
-                            />
-                        </label>
-                        {errors.name && <p>{errors.name.message}</p>}
 
-                        <label htmlFor="age">Leeftijd
-                            <input
-                                type="number"
-                                placeholder="0"
-                                {...register("age", {
-                                    max: {
-                                        value: 80,
-                                        message: "U mag maximaal 80 jaar oud zijn",
-                                    }
-                                })}
-                            />
-                        </label>
-                        {errors.age && <p>{errors.age.message}</p>}
+                        <div>
+                            <label htmlFor="name">Naam
+                                <input
+                                    type="text"
+                                    placeholder=""
+                                    {...register("name", {
+                                        required: "Naam mag niet leeg zijn",
+                                        validate: {
+                                            value: (value) => value.includes('@'),
+                                            message: "Naam mag geen @ bevatten",
+                                        },
+                                    })}
+                                />
+                            </label>
+                            {errors.name && <p>{errors.name.message}</p>}
+                        </div>
+
+                        <div>
+                            <label htmlFor="age">Leeftijd
+                                <input
+                                    type="number"
+                                    placeholder="0"
+                                    {...register("age", {
+                                        max: {
+                                            value: 80,
+                                            message: "U mag maximaal 80 jaar oud zijn",
+                                        }
+                                    })}
+                                />
+                            </label>
+                            {errors.age && <p>{errors.age.message}</p>}
+                        </div>
                     </fieldset>
                 </div>
+
 
                 <div className="form-container">
                     <fieldset>
                         <legend>Jouw review</legend>
-                        <label htmlFor="comments">Opmerkingen:
-                            <textarea
-                                {...register("comments", {
-                                    required: "Opmerking mag niet leeg zijn",
-                                    maxLength: {
-                                        value: 50,
-                                        message: "Er mogen maximaal 50 karakters gebruikt worden",
-                                    },
-                                })}
-                                rows="4"
-                                cols="35"
-                                placeholder="Wat vond je van het recept?"
+
+                        <div>
+                            <label htmlFor="comments">Opmerkingen:
+                                <textarea
+                                    {...register("comments", {
+                                        required: "Opmerking mag niet leeg zijn",
+                                        maxLength: {
+                                            value: 50,
+                                            message: "Er mogen maximaal 50 karakters gebruikt worden",
+                                        },
+                                    })}
+                                    rows="4"
+                                    cols="35"
+                                    placeholder="Wat vond je van het recept?"
+                                />
+                            </label>
+                            {errors.comments && <p>{errors.comments.message}</p>}
+                        </div>
+
+                        <div>
+                            <input
+                                type="checkbox"
+                                {...register("newsletter")}
                             />
-                        </label>
-                        {errors.comments && <p>{errors.comments.message}</p>}
+                            Ik schrijf me in voor de nieuwsbrief
+                        </div>
 
-                        <input
-                            type="checkbox"
-                            {...register("newsletter")}
-                        />
-                        Ik schrijf me in voor de nieuwsbrief
+                        <div>
+                            <label htmlFor="referrer">
+                                Hoe heb je dit recept gevonden?
+                                <select id="referrer" {...register("found-through")} >
+                                    <option value="google">Google</option>
+                                    <option value="friend">Vriend</option>
+                                    <option value="advertisement">Advertentie</option>
+                                    <option value="other">Anders</option>
+                                </select>
+                            </label>
 
-                        <label htmlFor="referrer">
-                            Hoe heb je dit recept gevonden?
-                            <select id="referrer" {...register("found-through")} >
-                                <option value="google">Google</option>
-                                <option value="friend">Vriend</option>
-                                <option value="advertisement">Advertentie</option>
-                                <option value="other">Anders</option>
-                            </select>
-                        </label>
+                            {selectedReferrer === "other" &&
+                            <input
+                                type="text"
+                                {...register("found-through-anders")}
+                            />
+                            }
+                        </div>
 
-                        {selectedReferrer === "other" &&
-                        <input
-                            type="text"
-                            {...register("found-through-anders")}
-                        />
-                        }
-
-                        <button
-                            type="submit"
-                        >
-                            Verstuur
-                        </button>
+                        <div>
+                            <button
+                                type="submit"
+                            >
+                                Verstuur
+                            </button>
+                        </div>
                     </fieldset>
                 </div>
 
